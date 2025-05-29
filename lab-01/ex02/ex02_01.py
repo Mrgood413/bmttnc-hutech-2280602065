8fe756f655c577ffb9906f980ad0abffcd3bf913
Mrgood413 <oanhdoanthi5@gmail.com>
2025-05-29 11:18:00 +0700

    buoi3

---
+++ lab-01/ex02/ex02_01.py
@@ -0,0 +1,4 @@
+ten = input('tên bạn là ')
+tuoi = input('tuổi bạn là ')
+
+print('chào mừng', ten ,'bạn',tuoi,"tuổi")
--- lab-01/ex02/ex02_02.py
+++ lab-01/ex02/ex02_02.py
@@ -1,4 +1,3 @@
-ten = input('tên bạn là ')
-tuoi = input('tuổi bạn là ')
-
-print('chào mừng', ten ,'bạn',tuoi,"tuổi")
+ban_kinh = float(input('vòng tròn có bán kính là '))
+dien_tich = 3.14 * (ban_kinh * 2)
+print('diện tích của hình tròn là',dien_tich)
